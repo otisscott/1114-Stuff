6e815a395343192b4f98963247f264716f544bd0
Otis Scott <otismscott@gmail.com>
2018-09-27 22:54:37 -0400

    Changed text.py

--- Class/test.py
+++ Class/test.py
@@ -1,2 +1,5 @@
-year = int(input("Enter a year: "))
-print(str(year) + " is a leap year" if year % 400 == 0 and year % 100 == 0 or year % 4 == 0 and year % 100 != 0 else str(year) + " is not a leap year")
+n = 0
+
+while n < 100:
+    print(n)
+    n += 3
